7726b22e17d46c21a60e1d4e58c0dff240a39363
Iulian Preda <iulian.octavian.preda@gmail.com>
2021-04-03 00:00:24 +0200

    Exported functions

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import {
+    getAllLocalStorage,
+    getAllManagedStorage,
+    getAllSyncStorage,
+    getFromLocalStorage,
+    getFromManagedStorage,
+    getFromStorage,
+    getFromSyncStorage
+} from './storage-get';
+import {
+    removeAllLocalStorage,
+    removeAllSyncStorage,
+    removeFromLocalStorage,
+    removeFromStorage,
+    removeFromSyncStorage
+} from './storage-remove';
+import {
+    saveManyToLocalStorage,
+    saveManyToStorage,
+    saveManyToSyncStorage,
+    saveToLocalStorage,
+    saveToStorage,
+    saveToSyncStorage
+} from './storage-save';
+
+export {
+    getAllLocalStorage,
+    getAllSyncStorage,
+    getAllManagedStorage,
+    getFromLocalStorage,
+    getFromSyncStorage,
+    getFromManagedStorage,
+    getFromStorage,
+    removeAllLocalStorage,
+    removeAllSyncStorage,
+    removeFromLocalStorage,
+    removeFromSyncStorage,
+    removeFromStorage,
+    saveManyToLocalStorage,
+    saveManyToSyncStorage,
+    saveToLocalStorage,
+    saveToSyncStorage,
+    saveToStorage,
+    saveManyToStorage
+};
